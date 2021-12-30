aadf0c20853f92b59dc5bbb52df50ac8fea4a9ea
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-12-30 23:33:52 +0330

    check before starting :|

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -11,7 +11,7 @@ const bot = new Telegraf(token);
 bot.telegram.setWebhook(url);
 
 bot.on("text", (ctx) => {
-  ctx.reply("just to be 100% sure");
+  ctx.reply("just to be 200% sure");
 });
 
 export default async function handler(req, res) {
