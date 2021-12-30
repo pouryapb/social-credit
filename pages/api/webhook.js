b84de66595536978599f9c690e061882345d20b9
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-12-30 22:00:15 +0330

    hello world to test if bot can run (2)

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,5 @@
 import { Telegraf } from "telegraf";
+import { json } from "micro";
 
 const token = process.env.BOT_TOKEN;
 const url = process.env.VERCEL_URL + "webhook";
@@ -7,15 +8,25 @@ if (token === undefined) {
   throw new Error("BOT_TOKEN must be provided!");
 }
 
-const bot = new Telegraf(token);
-
-// Set telegram webhook
-bot.telegram.setWebhook(url);
-
-const handler = bot.webhookCallback("/webhook");
+const bot = new Telegraf(token, { telegram: { webhookReply: false } });
 
 bot.on("text", (ctx) => {
   ctx.reply("Hello World!");
 });
 
-export default handler;
+bot.catch((err) => console.log("Ooops", err));
+
+module.exports = async function (req, res) {
+  try {
+    const body = await json(req);
+    console.log(body);
+    bot.handleUpdate(body);
+    res.statusCode = 200;
+    res.end("");
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Server Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e.message);
+  }
+};
