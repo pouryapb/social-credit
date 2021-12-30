843e1675537d3abab557e534a2563d94c08e0b6e
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-12-31 03:26:36 +0330

    moment of truth?

--- models/chat.js
+++ models/chat.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
+const memberSchema = Schema({
+  userId: { type: Number, unique: true },
+  username: { type: String, unique: true },
+  socialCredit: Number,
+});
+
 const chatSchema = Schema({
-  _id: Number,
+  chatId: { typeL: Number, unique: true },
   title: String,
-  members: [
-    {
-      _id: Number,
-      socialCredit: { type: Number, default: 100 },
-    },
-  ],
+  members: [memberSchema],
 });
 
 export default mongoose.models.Chat || model("Chat", chatSchema);
--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -23,11 +23,28 @@ bot.telegram.setWebhook(url);
 
 bot.on("sticker", (ctx) => {
   if (["group", "supergroup"].includes(ctx.chat.type)) {
-    ctx.replyWithMarkdownV2(
-      `message\\.reply_to_message: \`${JSON.stringify(
-        ctx.message.reply_to_message
-      )}\``
-    );
+    if (!ctx.message.reply_to_message) return;
+    if (![creditUpId, creditDownId].includes(ctx.message.sticker.file_id))
+      return;
+
+    const user = ctx.message.reply_to_message.from;
+    const dir = ctx.message.sticker.file_id === creditUpId ? 1 : -1;
+
+    if (Chat.findOne({ chatId: ctx.chat.id })) {
+      Chat.updateOne(
+        { "members.userId": user.id },
+        { $inc: { "members.$.socialCredit": 20 * dir } }
+      );
+    } else {
+      const chat = new Chat({
+        chatId: ctx.chat.id,
+        title: ctx.chat.title,
+        members: [
+          { userId: user.id, username: user.username, socialCredit: 20 * dir },
+        ],
+      });
+      chat.save();
+    }
   } else {
     ctx.reply("I only understand stickers in groups!");
   }
