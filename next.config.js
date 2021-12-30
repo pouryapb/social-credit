444b6a98232ef4311f376686b8848e36e6df2208
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-12-30 23:50:06 +0330

    yet another test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_PRODUCTION_BUILD } = require("next/constants");
+const { Telegraf } = require("telegraf");
+
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    const token = process.env.BOT_TOKEN;
+    const url = process.env.VERCEL_URL + "/api/webhook";
+    new Telegraf(token).telegram.setWebhook(url);
+  }
+
+  return {
+    reactStrictMode: true,
+  };
+};
