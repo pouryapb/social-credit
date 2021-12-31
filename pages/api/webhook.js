42538b7162d4ab0b54c74b1e07d44db89a22bd9a
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-12-31 16:29:23 +0330

    unneccessary logs removed and one test log added

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -5,8 +5,6 @@ import Chat from "../../models/chat";
 
 const token = process.env.BOT_TOKEN;
 const url = process.env.VERCEL_URL + "/api/webhook";
-const creditUpId = process.env.CREDIT_UP_ID;
-const creditDownId = process.env.CREDIT_DOWN_ID;
 
 if (token === undefined) {
   throw new Error("BOT_TOKEN must be provided!");
@@ -16,6 +14,9 @@ const bot = new Telegraf(token);
 bot.telegram.setWebhook(url);
 
 bot.on("sticker", async (ctx) => {
+  const creditUpId = process.env.CREDIT_UP_ID;
+  const creditDownId = process.env.CREDIT_DOWN_ID;
+
   if (["group", "supergroup"].includes(ctx.chat.type)) {
     if (!ctx.message.reply_to_message) {
       console.log("No reply message");
@@ -25,26 +26,24 @@ bot.on("sticker", async (ctx) => {
       ![creditUpId, creditDownId].includes(ctx.message.sticker.file_unique_id)
     ) {
       console.log("Not a credit sticker");
-      ctx.reply(
-        `sticker id: ${ctx.message.sticker.file_unique_id}\nenv up: ${creditUpId}\nenv down: ${creditDownId}`
-      );
       return;
     }
 
     await dbConnect();
-    console.log("Connected to DB");
 
     const user = ctx.message.reply_to_message.from;
     const dir = ctx.message.sticker.file_unique_id === creditUpId ? 1 : -1;
-    const c = await Chat.findOne({ chatId: ctx.chat.id }).exec();
-    if (c) {
-      console.log("Chat found");
+
+    if (await Chat.findOne({ chatId: ctx.chat.id }).exec()) {
       Chat.findOneAndUpdate(
         { "members.userId": user.id },
         { $inc: { "members.$.socialCredit": 20 * dir } }
-      ).exec();
+      )
+        .exec()
+        .then((result) => {
+          console.log(result);
+        });
     } else {
-      console.log("Chat not found");
       const chat = new Chat({
         chatId: ctx.chat.id,
         title: ctx.chat.title,
@@ -52,7 +51,12 @@ bot.on("sticker", async (ctx) => {
           { userId: user.id, username: user.username, socialCredit: 20 * dir },
         ],
       });
-      chat.save();
+      await chat.save();
+      ctx.reply(
+        `@${user.username}'s Social Credit was ${
+          dir > 0 ? "increased" : "decreased"
+        } to ${20 * dir}!`
+      );
     }
   } else {
     ctx.reply("I only understand stickers in groups!");
