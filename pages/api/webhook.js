37c5b31723b2e9fcb63e0ff05b05f1ef07fcb8cc
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2022-01-07 17:38:31 +0330

    promise

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -46,7 +46,7 @@ bot.on("sticker", async (ctx) => {
       ).socialCredit;
 
       ctx.reply(
-        `@${user.first_name}'s Social Credit was ${
+        `${user.first_name}'s Social Credit was ${
           dir > 0 ? "increased" : "decreased"
         } to ${currentCredit}!`
       );
@@ -99,7 +99,7 @@ bot.on("sticker", async (ctx) => {
     await chat.save();
 
     ctx.reply(
-      `@${user.first_name}'s Social Credit was ${
+      `${user.first_name}'s Social Credit was ${
         dir > 0 ? "increased" : "decreased"
       } to ${20 * dir}!`
     );
@@ -121,13 +121,13 @@ bot.command("score", async (ctx) => {
   const user = ctx.message.reply_to_message.from;
   const chat = await Chat.findOne({ chatId: ctx.chat.id }).exec();
 
-  if (!chat) return ctx.reply(`@${user.first_name}'s Social Credit is 0!`);
+  if (!chat) return ctx.reply(`${user.first_name}'s Social Credit is 0!`);
 
   const member = chat.members.find((m) => m.userId === user.id);
 
-  if (!member) return ctx.reply(`@${user.first_name}'s Social Credit is 0!`);
+  if (!member) return ctx.reply(`${user.first_name}'s Social Credit is 0!`);
 
-  ctx.reply(`@${user.first_name}'s Social Credit is ${member.socialCredit}!`);
+  ctx.reply(`${user.first_name}'s Social Credit is ${member.socialCredit}!`);
 });
 
 bot.command("list", async (ctx) => {
@@ -141,25 +141,27 @@ bot.command("list", async (ctx) => {
   if (!chat)
     return ctx.reply("All members in this group have 0 Social Credit!");
 
-  const members = await chat.members
-    .sort((a, b) => b.socialCredit - a.socialCredit)
-    .map(async (m, index) => {
-      let firstName = m.first_name;
+  const members = await Promise.all(
+    chat.members
+      .sort((a, b) => b.socialCredit - a.socialCredit)
+      .map(async (m, index) => {
+        let firstName = m.first_name;
 
-      if (!firstName) {
-        const member = await ctx.getChatMember(m.userId);
-        firstName = member.user.first_name;
-        await updateUser(m.userId, firstName);
-      }
+        if (!firstName) {
+          const member = await ctx.getChatMember(m.userId);
+          firstName = member.user.first_name;
+          await updateUser(m.userId, firstName);
+        }
 
-      let rank = index + 1;
-      if (rank === 1) rank = "🥇.";
-      else if (rank === 2) rank = "🥈.";
-      else if (rank === 3) rank = "🥉.";
-      else rank = `${rank}.`;
+        let rank = index + 1;
+        if (rank === 1) rank = "🥇.";
+        else if (rank === 2) rank = "🥈.";
+        else if (rank === 3) rank = "🥉.";
+        else rank = `${rank}.`;
 
-      return `${rank} @${firstName} (${m.socialCredit})`;
-    });
+        return `${rank} ${firstName} (${m.socialCredit})`;
+      })
+  );
 
   const totalMembers = await ctx.getChatMembersCount();
 
